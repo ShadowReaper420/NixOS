a85e3481d5f0488dd33a5425b83d6c84e7c5eeb7
Shadowreaper <146676106+ShadowReaper420@users.noreply.github.com>
2024-08-14 11:47:18 -0400

    Add files via upload
    Fucking kill me

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -7,17 +7,21 @@
     inputs = {
 
       nixpkgs.url = "nixpkgs/nixos-unstable";
+      home-manager.url ="github:nix-community/home-manager/master";
+      home-manager.inputs,nixpkgs.follows = "nixpkgs";
     };
 
 
-    outputs = {self, nixpkgs, ...}:
+    outputs = {self, nixpkgs, home-manager, ...}:
        let
          lib = nixpkgs.lib;
+         system = "x86_64-linux";
+         pkgs = nixpkgs.legacyPackages.${system};
       in {
  
       nixosConfigurations = {
          nixos = lib.nixosSystem{
-            system = "x86_64-linux";
+            inherit system;
             modules = [./configuration.nix];
 
 
@@ -27,6 +31,14 @@
 
       };
     };
+    homeConfigurations ={
+         flugel = home-manager.lib.homeManagerConfiguration{
+            inherit pkgs;
+            modules = [./home.nix];
+         };
+    };
+  };
 
 
-    }
+  }
+
