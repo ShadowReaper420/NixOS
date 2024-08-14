d77f4a2977cd0110780ab42dc70d0c52ef16dea7
Shadowreaper <146676106+ShadowReaper420@users.noreply.github.com>
2024-08-14 08:41:34 -0400

    Add files via upload

---
+++ nixos/flake.nix
@@ -0,0 +1,32 @@
+{
+
+
+    description = "Control Flake";
+
+
+    inputs = {
+
+      nixpkgs.url = "nixpkgs/nixos-unstable";
+    };
+
+
+    outputs = {self, nixpkgs, ...}:
+       let
+         lib = nixpkgs.lib;
+      in {
+ 
+      nixosConfigurations = {
+         nixos = lib.nixosSystem{
+            system = "x86_64-linux";
+            modules = [./configuration.nix];
+
+
+         };
+
+
+
+      };
+    };
+
+
+    }
