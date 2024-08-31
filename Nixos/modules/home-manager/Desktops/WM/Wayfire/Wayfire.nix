4f28f33f2fdade069dab27440c946e92bfb40bfb
Shadowreaper <shadowreaper26@proton.me>
2024-08-30 22:26:38 -0400

    This probably wont work.

--- Nixos/modules/home-manager/Desktops/WM/Wayfire/Wayfire.nix
+++ Nixos/modules/home-manager/Desktops/WM/Wayfire/Wayfire.nix
@@ -1,7 +1,28 @@
 {lib, config, pkgs, ...}:
 
 {
-     xdg.configFile. "./wayfire/wayfire.ini".source = ./Dotfiles/wayfire.ini;
-    };
+  programs.wayfire = {
+  enable = true;
+  
+  
+  xdg.configFile. "./wayfire/wayfire.ini".source = ./Dotfiles/wayfire.ini;
+  plugins = with pkgs.wayfirePlugins; [
+    wcm
+    wf-shell
+    wayfire-plugins-extra
+  ];
+  services.xserver = {
+    displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+      #theme = ""
+       package = pkgs.sddm;
+      
 
-  };
+      };
+   
+
+
+
+    };
+};
--- Nixos/modules/home-manager/default.nix
+++ Nixos/modules/home-manager/default.nix
@@ -10,7 +10,7 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
-      #./Desktops/WM/Wayfire/Wayfire.nix
+      ./Desktops/WM/Wayfire/Wayfire.nix
       ./Home/Gaming.nix
       #./Desktops/WM/Wayland-Utils/Wayland-utils.nix
       ./Utils/Utils.nix
