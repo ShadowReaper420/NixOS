34de7319d43ad75c432c061d6694f636ba8b3737
ShadowReaper <William_Regulus@proton.me>
2024-09-30 20:51:55 -0400

    LFS is easier than this shit.

--- .Dotfiles/System/configuration.nix
+++ .Dotfiles/System/configuration.nix
@@ -70,16 +70,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.flugel = {
@@ -106,7 +97,6 @@
     git
     nil
     vscodium
-    spice-vdagent
   ];
   home-manager = {
     extraSpecialArgs = {
